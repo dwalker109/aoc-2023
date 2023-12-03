7ea5c0d59fb7226af094903f20d7a5761ae77510
Dan Walker <dwalker109@gmail.com>
2023-12-03 06:54:37 +0000

    Day 3: Gear Ratios (optimisation, CI, readme)

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -9,29 +9,28 @@ fn main() {
 }
 
 fn part1(input: &'static str) -> Answer {
-    let symbols = parse_symbols(input);
+    let symbols = parse_symbols(input).collect::<HashMap<_, _>>();
     let numbers = parse_numbers(input);
 
     numbers
-        .iter()
         .filter(|pn| pn.adjacents.iter().any(|xy| symbols.contains_key(xy)))
         .map(|pn| pn.num)
         .sum()
 }
 
 fn part2(input: &'static str) -> Answer {
-    let gears = parse_symbols(input)
-        .into_iter()
-        .filter(|(_, c)| *c == '*')
-        .collect::<HashMap<_, _>>();
-    let numbers = parse_numbers(input);
+    let symbols = parse_symbols(input);
+    let numbers = parse_numbers(input).collect::<Vec<_>>();
 
-    gears
-        .iter()
-        .filter_map(|(xy, _)| {
+    symbols
+        .filter_map(|(xy, c)| {
+            if c != '*' {
+                return None;
+            }
+
             let pns = numbers
                 .iter()
-                .filter(|pn| pn.adjacents.contains(xy))
+                .filter(|pn| pn.adjacents.contains(&xy))
                 .collect::<Vec<_>>();
 
             if pns.len() == 2 {
@@ -43,59 +42,51 @@ fn part2(input: &'static str) -> Answer {
         .sum()
 }
 
-fn parse_symbols(input: &str) -> HashMap<Xy, char> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, l)| {
-            l.chars().enumerate().filter_map(move |(x, c)| {
-                (!c.is_numeric() && c != '.')
-                    .then_some((Xy(x.try_into().unwrap(), y.try_into().unwrap()), c))
-            })
+fn parse_symbols(input: &str) -> impl Iterator<Item = (Xy, char)> + '_ {
+    input.lines().enumerate().flat_map(|(y, l)| {
+        l.chars().enumerate().filter_map(move |(x, c)| {
+            (!c.is_numeric() && c != '.')
+                .then_some((Xy(x.try_into().unwrap(), y.try_into().unwrap()), c))
         })
-        .collect()
+    })
 }
 
-fn parse_numbers(input: &str) -> Vec<MaybePartNumber> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, l)| {
-            let seq = l
-                .chars()
-                .enumerate()
-                .filter(|(_, c)| c.is_numeric())
-                .collect::<HashMap<_, _>>();
+fn parse_numbers(input: &str) -> impl Iterator<Item = MaybePartNumber> + '_ {
+    input.lines().enumerate().flat_map(|(y, l)| {
+        let seq = l
+            .chars()
+            .enumerate()
+            .filter(|(_, c)| c.is_numeric())
+            .collect::<HashMap<_, _>>();
 
-            let mut collected = Vec::new();
-            let mut curr_seq = Vec::new();
-            let mut curr_val = String::new();
+        let mut collected = Vec::new();
+        let mut curr_seq = Vec::new();
+        let mut curr_val = String::new();
 
-            for x in 0_usize..l.len() {
-                if let Some(c) = seq.get(&x) {
-                    curr_seq.push(Xy(x.try_into().unwrap(), y.try_into().unwrap()));
-                    curr_val.push(*c);
+        for x in 0_usize..l.len() {
+            if let Some(c) = seq.get(&x) {
+                curr_seq.push(Xy(x.try_into().unwrap(), y.try_into().unwrap()));
+                curr_val.push(*c);
 
-                    if !seq.contains_key(&(x + 1)) {
-                        let adjacents = curr_seq
-                            .iter()
-                            .flat_map(|xy| xy.neighbours())
-                            .collect::<HashSet<_>>();
+                if !seq.contains_key(&(x + 1)) {
+                    let adjacents = curr_seq
+                        .iter()
+                        .flat_map(|xy| xy.neighbours())
+                        .collect::<HashSet<_>>();
 
-                        collected.push(MaybePartNumber {
-                            num: curr_val.parse().unwrap(),
-                            adjacents,
-                        });
+                    collected.push(MaybePartNumber {
+                        num: curr_val.parse().unwrap(),
+                        adjacents,
+                    });
 
-                        curr_seq.clear();
-                        curr_val.clear();
-                    }
+                    curr_seq.clear();
+                    curr_val.clear();
                 }
             }
+        }
 
-            collected
-        })
-        .collect()
+        collected
+    })
 }
 
 #[derive(Eq, PartialEq, Hash, Clone, Copy)]
