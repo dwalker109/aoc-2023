6f7fc29e3a86711061e46fb6f18e1d1b91a37d08
Dan Walker <dan.walker@jagex.com>
2023-12-06 05:15:48 +0000

    Day 5: If You Give A Seed A Fertilizer (2)

--- days/day05/src/main.rs
+++ days/day05/src/main.rs
@@ -1,9 +1,12 @@
-use std::collections::HashMap;
+#![feature(array_chunks)]
+
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::collections::HashSet;
 use std::ops::Range;
 
 static INPUT: &str = include_str!("../../../input/day05");
 
-type Answer = usize;
+type Answer = isize;
 
 fn main() {
     aoc_shared::runner::solve(|| part1(INPUT), || part2(INPUT))
@@ -17,7 +20,7 @@ fn part1(input: &'static str) -> Answer {
         .map(|&s| {
             maps.iter().fold(s, |acc, cur| {
                 for sm in cur {
-                    if let Some(result) = sm.map(acc) {
+                    if let Some(result) = sm.map_one(acc) {
                         return result;
                     }
                 }
@@ -30,10 +33,46 @@ fn part1(input: &'static str) -> Answer {
 }
 
 fn part2(input: &'static str) -> Answer {
-    todo!();
+    let (seeds, maps) = parse(input);
+
+    // let seeds = [82, 1];
+
+    seeds
+        .array_chunks()
+        .map(|[start, len]| *start..(start + len))
+        .flat_map(|r| {
+            maps.iter()
+                .fold([r].into_iter().collect::<HashSet<_>>(), |acc, cur| {
+                    let mut next = HashSet::new();
+                    let mut unchecked = acc.into_iter().collect::<Vec<_>>();
+
+                    for sm in cur {
+                        let mut next_unchecked = Vec::new();
+
+                        while let Some(sr) = unchecked.pop() {
+                            let (mapped, unmapped) = sm.map_range(sr.clone());
+
+                            if let Some(mapped) = mapped {
+                                next.extend(mapped);
+                            }
+
+                            if let Some(unmapped) = unmapped {
+                                next_unchecked.extend(unmapped);
+                            }
+                        }
+
+                        unchecked = next_unchecked;
+                    }
+
+                    next.into_iter().chain(unchecked).collect()
+                })
+        })
+        .map(|r| r.start)
+        .min()
+        .unwrap()
 }
 
-fn parse(input: &str) -> (Vec<usize>, Vec<Vec<ConvMap>>) {
+fn parse(input: &str) -> (Vec<isize>, Vec<Vec<ConvMap>>) {
     let mut sections = input.split("\n\n");
 
     let seeds = sections
@@ -43,7 +82,7 @@ fn parse(input: &str) -> (Vec<usize>, Vec<Vec<ConvMap>>) {
         .unwrap()
         .1
         .split_ascii_whitespace()
-        .filter_map(|n| n.parse::<usize>().ok())
+        .filter_map(|n| n.parse::<isize>().ok())
         .collect::<Vec<_>>();
 
     let maps = sections
@@ -53,29 +92,84 @@ fn parse(input: &str) -> (Vec<usize>, Vec<Vec<ConvMap>>) {
     (seeds, maps)
 }
 
-struct ConvMap(Range<usize>, isize);
+struct ConvMap(Range<isize>, isize);
 
 impl From<&str> for ConvMap {
     fn from(value: &str) -> Self {
         let mut vals = value.splitn(3, ' ');
-        let dest_start = vals.next().unwrap().parse::<usize>().unwrap();
-        let src_start = vals.next().unwrap().parse::<usize>().unwrap();
-        let len = vals.next().unwrap().parse::<usize>().unwrap();
+        let dest_start = vals.next().unwrap().parse::<isize>().unwrap();
+        let src_start = vals.next().unwrap().parse::<isize>().unwrap();
+        let len = vals.next().unwrap().parse::<isize>().unwrap();
 
-        let src_range = (src_start..src_start + len);
-        let offset = dest_start as isize - src_start as isize;
+        let src_range = src_start..src_start + len;
+        let offset = dest_start - src_start;
 
         Self(src_range, offset)
     }
 }
 
 impl ConvMap {
-    fn map(&self, src: usize) -> Option<usize> {
-        if self.0.contains(&src) {
-            return Some((src as isize + self.1) as usize);
+    fn map_one(&self, target: isize) -> Option<isize> {
+        if self.0.contains(&target) {
+            return Some(target + self.1);
         }
 
         None
+    }
+
+    fn map_range(
+        &self,
+        lookup: Range<isize>,
+    ) -> (Option<Vec<Range<isize>>>, Option<Vec<Range<isize>>>) {
+        let (map_from, offset) = (&self.0, self.1);
+
+        let offset_range = |start: isize, end: isize| {
+            let s = start + offset;
+            let e = end + offset;
+
+            s..e
+        };
+
+        match (
+            lookup.start.cmp(&map_from.start),
+            lookup.end.cmp(&map_from.start),
+            lookup.start.cmp(&map_from.end),
+            lookup.end.cmp(&map_from.end),
+        ) {
+            (Less, Less | Equal, _, _) => {
+                //all before
+                (None, Some(vec![lookup]))
+            }
+            (_, _, Greater | Equal, Greater) => {
+                // all after
+                (None, Some(vec![lookup]))
+            }
+            (Greater | Equal, _, _, Less | Equal) => {
+                // all within
+                (Some(vec![offset_range(lookup.start, lookup.end)]), None)
+            }
+            (Less, _, _, Less | Equal) => {
+                // start before, end within
+                (
+                    Some(vec![offset_range(map_from.start, lookup.end)]),
+                    Some(vec![lookup.start..map_from.start]),
+                )
+            }
+            (Greater | Equal, _, _, Greater) => {
+                // start within, end after
+                (
+                    Some(vec![offset_range(lookup.start, map_from.end)]),
+                    Some(vec![map_from.end..lookup.end]),
+                )
+            }
+            (Less, _, _, Greater) => {
+                // start before, end after
+                (
+                    Some(vec![offset_range(map_from.start, map_from.end)]),
+                    Some(vec![lookup.start..map_from.start, map_from.end..lookup.end]),
+                )
+            }
+        }
     }
 }
 
@@ -90,6 +184,6 @@ mod tests {
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(INPUT), super::Answer::default());
+        assert_eq!(super::part2(INPUT), 46);
     }
 }
