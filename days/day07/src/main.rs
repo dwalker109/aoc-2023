62ca88844836512f268b8b1a61bf1aed6e0aaa33
Dan Walker <dan.walker@jagex.com>
2023-12-07 06:30:49 +0000

    Day 7: Camel Cards (2)

--- days/day07/src/main.rs
+++ days/day07/src/main.rs
@@ -1,6 +1,4 @@
 use itertools::Itertools;
-use std::cmp::Ordering;
-use std::collections::HashMap;
 
 static INPUT: &str = include_str!("../../../input/day07");
 
@@ -13,7 +11,7 @@ fn main() {
 fn part1(input: &'static str) -> Answer {
     input
         .lines()
-        .map(Hand::from)
+        .map(reg::Hand::from)
         .sorted()
         .rev()
         .enumerate()
@@ -22,81 +20,18 @@ fn part1(input: &'static str) -> Answer {
 }
 
 fn part2(input: &'static str) -> Answer {
-    todo!();
-}
-
-#[derive(PartialEq, Eq, Debug)]
-struct Hand {
-    raw: String,
-    num: (usize, usize, usize, usize, usize),
-    bid: usize,
-    strength: usize,
-}
-
-impl From<&str> for Hand {
-    fn from(value: &str) -> Self {
-        let (cards, bid) = value.split_once(' ').unwrap();
-
-        let tr8 = [
-            'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
-        ]
-        .iter()
+    input
+        .lines()
+        .map(jkr::Hand::from)
+        .sorted()
+        .rev()
         .enumerate()
-        .map(|(i, c)| (*c, i))
-        .collect::<HashMap<_, _>>();
-
-        let num = cards.chars().map(|c| *tr8.get(&c).unwrap()).collect_vec();
-
-        let g = cards
-            .chars()
-            .counts()
-            .into_iter()
-            .map(|(c, i)| i)
-            .sorted()
-            .rev()
-            .collect_vec();
-
-        let strength = match g.len() {
-            1 => 1,
-            2 => match (g[0], g[1]) {
-                (4, 1) => 2,
-                (3, 2) => 3,
-                _ => panic!(),
-            },
-            3 => match (g[0], g[1], g[2]) {
-                (3, 1, 1) => 4,
-                (2, 2, 1) => 5,
-                _ => panic!(),
-            },
-            4 => 6,
-            5 => 7,
-            _ => panic!(),
-        };
-
-        Self {
-            raw: cards.to_string(),
-            num: (num[0], num[1], num[2], num[3], num[4]),
-            bid: bid.parse().unwrap(),
-            strength,
-        }
-    }
+        .map(|(r, h)| h.bid * (r + 1))
+        .sum()
 }
 
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.strength.cmp(&other.strength) {
-            Ordering::Less => Ordering::Less,
-            Ordering::Greater => Ordering::Greater,
-            Ordering::Equal => self.num.cmp(&other.num),
-        }
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+mod jkr;
+mod reg;
 
 #[cfg(test)]
 mod tests {
@@ -109,6 +44,6 @@ mod tests {
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(INPUT), super::Answer::default());
+        assert_eq!(super::part2(INPUT), 5905);
     }
 }
---
+++ days/day07/src/reg.rs
@@ -0,0 +1,76 @@
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Hand {
+    raw: String,
+    num: (usize, usize, usize, usize, usize),
+    pub bid: usize,
+    strength: usize,
+}
+
+impl From<&str> for Hand {
+    fn from(value: &str) -> Self {
+        let (cards, bid) = value.split_once(' ').unwrap();
+
+        let tr8 = [
+            'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
+        ]
+        .iter()
+        .enumerate()
+        .map(|(i, c)| (*c, i))
+        .collect::<HashMap<_, _>>();
+
+        let num = cards.chars().map(|c| *tr8.get(&c).unwrap()).collect_vec();
+
+        let g = cards
+            .chars()
+            .counts()
+            .into_iter()
+            .map(|(c, i)| i)
+            .sorted()
+            .rev()
+            .collect_vec();
+
+        let strength = match g.len() {
+            1 => 1,
+            2 => match (g[0], g[1]) {
+                (4, 1) => 2,
+                (3, 2) => 3,
+                _ => panic!(),
+            },
+            3 => match (g[0], g[1], g[2]) {
+                (3, 1, 1) => 4,
+                (2, 2, 1) => 5,
+                _ => panic!(),
+            },
+            4 => 6,
+            5 => 7,
+            _ => panic!(),
+        };
+
+        Self {
+            raw: cards.to_string(),
+            num: (num[0], num[1], num[2], num[3], num[4]),
+            bid: bid.parse().unwrap(),
+            strength,
+        }
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.strength.cmp(&other.strength) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => self.num.cmp(&other.num),
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
